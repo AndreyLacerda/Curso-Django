709047168d84c50b33cb4a4e131442066c0e8c4a
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-04-13 10:33:58 -0300

    tirando static_dir das settings

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -161,9 +161,4 @@ django_heroku.settings(locals())
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
