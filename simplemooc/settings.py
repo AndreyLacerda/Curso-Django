a500ba590f337e2d05c02442ad83a51462821532
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-04-13 10:30:52 -0300

    Alterando settings.py

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -156,11 +156,9 @@ ALLOWED_HOSTS = ['*']
 # Activate Django-Heroku.
 django_heroku.settings(locals())
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
