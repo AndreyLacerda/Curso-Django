fda327ac92b0baa59bdb4329325d32ef87c74132
AndreyLacerda <andrey-lacerda@hotmail.com>
2020-04-15 11:43:04 -0300

    Subindo código final

--- simplemooc/forum/urls.py
+++ simplemooc/forum/urls.py
@@ -4,5 +4,9 @@ from . import views
 app_name = 'forum'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('tag/<slug:tag>/', views.index, name='index_tagged'),
+    path('respostas/<slug:pk>/correta/', views.reply_correct, name='reply_correct'),
+    path('respostas/<slug:pk>/incorreta/', views.reply_incorrect, name='reply_incorrect'),
+    path('<slug:slug>/', views.thread, name='thread')
 ]
